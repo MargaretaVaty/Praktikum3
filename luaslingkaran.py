cd7ecfc2cd70a9da747fbfcd9b171cea15334def
MargaretaVaty <117182720+MargaretaVaty@users.noreply.github.com>
2022-11-01 20:57:47 +0700

    add file latihan3.py latihan1.py dan luaslingkaran

---
+++ luaslingkaran.py
@@ -0,0 +1,6 @@
+phi = 3.14
+r = float(input("Masukkan panjang jari-jari lingkaran: "))
+luas = phi*r*r
+print("Luas lingkaran adalah : "+ str(luas))
+k = 2 * phi * r
+print("Keliling Lingkaran adalah {}".format(k))
